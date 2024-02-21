229d42b48c9b8932fe939469ad10ae3604c2dfde
Filip05-TUES <f.ermenkov@gmail.com>
2024-02-21 13:17:11 +0200

    Commit the current progress
    - All eight puzzles are designed and implemented
    - Player can die and respawn in their base
    - Player can interact with the NPC
    - Design and implement a boss battle - not finished

---
+++ Source/QuantumOdyssey/QuantumOdyssey.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "QuantumOdyssey.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuantumOdyssey, "QuantumOdyssey" );
